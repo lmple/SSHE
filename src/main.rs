86132244e9d9d7740bf05225547003c45b2bb1f6
lmple <llievre@protonmail.com>
2022-06-18 21:59:34 +0200

    no warning but stdin problem

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,30 @@ use core::fmt::Write;
 use std::env;
 use std::path::Path;
 
-fn printlines(start : usize, end : usize, v : Vec<Vec<u8>>){
+fn printlines(start : usize, end : usize, v : &Vec<Vec<u8>>){
     for l in start..end+1 {
         let line = &v[l];
 
         let mut str_line = String::new();
 
         for byte in line {
-            write!(str_line, "{:02x} ", byte);
+            write!(str_line, "{:02x} ", byte).ok();
         }
-
 
         println!("{}: {}", l, str_line);
     }
+
+    //print an empty lines after
+    println!("");
+}
+
+fn show_commands(){
+    println!("u -> go to previous line");
+    println!("d -> go to next line");
+    println!("exit -> exit the program");
+
+    //print an empty lines after
+    println!("");
 }
 
 fn main() {
@@ -40,12 +51,15 @@ fn main() {
     let mut count_per_line : usize = 0;
     let max_per_line : usize = 12;
 
+    //all lines
     let mut bytes_lines : Vec<Vec<u8>> = Vec::new();
 
     let bytes = buffer.bytes();
 
+    //a line of N bytes
     let mut bytes_line : Vec<u8> = Vec::new();
 
+    //create array with bytes from the file
     for byte_or_error in bytes {
         let byte = byte_or_error.unwrap();
         bytes_line.push(byte);
@@ -57,5 +71,36 @@ fn main() {
         }
     }
 
-    printlines(0, 3, bytes_lines);
+
+    let start_line = 0;
+
+
+    let stdin = std::io::stdin();
+
+    loop {
+        //print first ten lines
+        printlines(start_line, 9, &bytes_lines);
+
+        print!(">> ");
+
+        //flush to avoid problem with priting
+
+        // recreated everytime to avoid stdin problems
+        let mut user_command : String = String::new();
+
+        //get user input
+        stdin.read_line(&mut user_command).ok();
+
+        //get clean command and match with good command
+        let cleaned_command = user_command.trim();
+        match cleaned_command {
+            "u" => println!("up"),
+            "d" => println!("down"),
+            "h" => show_commands(),
+            "exit" => break,
+            _ => println!("Unknown command")
+        }
+
+        println!("Bye");
+    }
 }
